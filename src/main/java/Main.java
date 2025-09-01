116bba693dad56965f1d1b833bdc3c243f7446c3
saidm <smartelod@unicartagena.edu.co>
2025-09-01 13:26:55 -0500

    Se implementaron los simbolos y ventanas emergentes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,24 @@
+import javax.swing.*;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("-- Generador de Contraseñas --");
-        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         // arreglos de caracteres
         char[] mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         char[] minusculas = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         char[] numeros = "0123456789".toCharArray();
+        char[] simbolos = "!@#$%^&*()_+-=[]{}".toCharArray();
 
         // pide longitud de la contraseña
-        System.out.println("Ingrese la longitud de la contraseña (Minimo 8)");
-        int longitud = scanner.nextInt();
+        String input = JOptionPane.showInputDialog("Ingrese la longitud de la contraseña (mínimo 4):");
+        int longitud = Integer.parseInt(input);
 
         if (longitud < 8) {
-            System.out.println("La longitud debe ser mayor o igual a 8");
+            JOptionPane.showMessageDialog(null, "La longitud debe ser mayor o igual a 8");
+            return;
         }
 
         // array para guardar la contraseña
@@ -27,17 +28,19 @@ public class Main {
         contrasena[0] = mayusculas[random.nextInt(mayusculas.length)];
         contrasena[1] = minusculas[random.nextInt(minusculas.length)];
         contrasena[2] = numeros[random.nextInt(numeros.length)];
+        contrasena[3] = simbolos[random.nextInt(simbolos.length)];
 
         // une todos los arreglos en un solo string
-        String todos = new String(mayusculas) + new String(minusculas) + new String(numeros);
+        String todos = new String(mayusculas) + new String(minusculas)
+                + new String(numeros) + new String(simbolos);
         char[] caracteres = todos.toCharArray();
 
-        // comptleta el resto de la contraseña
-        for (int i = 8; i < longitud; i++) {
+        // completa el resto de la contraseña
+        for (int i = 4; i < longitud; i++) {
             contrasena[i] = caracteres[random.nextInt(caracteres.length)];
         }
 
-        // mezcla los caracteres del principio para que no siempre empiece en el mismo orden
+        // mezcla los caracteres para que no quede predecible
         for (int i = 0; i < contrasena.length; i++) {
             int j = random.nextInt(contrasena.length);
             char temp = contrasena[i];
@@ -45,6 +48,7 @@ public class Main {
             contrasena[j] = temp;
         }
 
-        System.out.println("Contraseña generada: " + new String(contrasena));
+        // mostrar la contraseña generada
+        JOptionPane.showMessageDialog(null, "Contraseña generada:\n" + new String(contrasena));
     }
 }
